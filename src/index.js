7d2992c0df213aec7ff55030e690aa97b260aabb
GuiSilva011 <114703420+GuiSilva011@users.noreply.github.com>
2023-04-15 10:37:15 -0300

    Criado Rotas

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
-const express = require('express')
+import * as  express from 'express'
+
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World 2 ')
+  res.send('GET /')
 })
 
-app.listen(3000, () => console.log(`ONLINE https://localhost:3000`))
+app.post ('/', (req, res) => {
+  res.status(201).send('POST /')
+})
+app.put ('/', (req, res) => {
+  res.send('PUT /')
+})
+app.delete ('/', (req, res) => {
+  res.send('DELETE /')
+})
+app.path ('/', (req, res) => {
+  res.send('PATH /')
+})
+
+app.listen(3000, () => console.log(`ONLINE http://localhost:3000`))
 
